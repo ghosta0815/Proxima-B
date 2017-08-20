877ea286992b086d0ba6747a26473bcc4163a726
ghosta0815 <Manuel.Gschrey@gmail.com>
2017-08-20 12:39:06 +0200

    Protoype for starting a WPF Application from Main Method

---
+++ Prototypes/Prototype_MinimalWPFCall/Prototype_MinimalWPFCall/MainStartup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace Prototype_MinimalWPFCall
+{  
+    class MainStartup
+    {
+        //Required to uise COM components (indicates that application is single-threaded
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            //Requires System.PresentationFramework for System.Windows.Application
+            Application app = new Application();
+            app.Run(new MainWindow());
+        }
+    }
+}
